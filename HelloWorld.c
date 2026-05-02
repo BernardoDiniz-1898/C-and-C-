01ed8b565a4d59f1d189792c5865b78abc87c525
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 <BernardoDiniz-1898>
2026-05-01 22:16:09 -0300

    Primeiro Commit

---
+++ HelloWorld.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int helloname() {
+  char nome[15];
+  printf("Insira seu nome completo: ");
+  scanf("%s", nome);
+  printf("Hello World %s!", nome);
+  return 0;
+}
+
+int main() {
+  printf("Hello World!\n\n");
+  printf("Gostaria de se nome printado na tela com um 'Hello World'ao lado?"
+         "(s/n)\n");
+  char descisao;
+  printf("-> ");
+  scanf("%c", &descisao);
+
+  if (descisao == 's') {
+    system("clear");
+    helloname();
+  }
+
+  return 0;
+}
---
+++ ManipulandoNumeros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int SomaBasica() {
+  float x;
+  float y;
+  printf("digite o valor de x: ");
+  scanf("%f", &x);
+  printf("Digite o valor de y: ");
+  scanf("%f", &y);
+  return x + y;
+}
+
+int main() {
+  printf("O resultado é %ḍ̣̣̣\n", SomaBasica());
+  return 0;
+}
