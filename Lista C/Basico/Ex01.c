255299e45b3e7b9e77cb8c98b13fed97d986cade
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 <BernardoDiniz-1898>
2026-05-02 23:03:59 -0300

    inicio dos exercicios

---
+++ Lista C/Basico/Ex01.c
@@ -0,0 +1,7 @@
+// 1. Faça um programa em C que imprima a mensagem “Hello, world!"
+#include <stdio.h>
+
+int main() {
+  printf("\n\nHello World!\n\n");
+  return 0;
+}
---
+++ Lista C/Basico/Ex02.c
@@ -0,0 +1,15 @@
+// 2. Tipos de Dados - Faça um programa em C que calcule a soma das variáveis
+// definidas abaixo e apresente o resultado em tela int a = 3;
+// float b = 4.5;
+// double c = 5.25;
+// float sum
+#include <stdio.h>
+
+int main() {
+  int a = 3;
+  float b = 4.5;
+  double c = 5.25;
+  float sum = a + b + c;
+  printf("%f", sum);
+  return 0;
+}
